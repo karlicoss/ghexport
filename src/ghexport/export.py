3c60e930b7784a4a623c91a0fa5a506fcfc80033
karlicoss <karlicoss@gmail.com>
2023-10-20 17:38:40 +0100

    export: add CLI flags to control data to be exported
    see readme for more details

--- src/ghexport/export.py
+++ src/ghexport/export.py
@@ -1,91 +1,130 @@
 #!/usr/bin/env python3
 import argparse
 import json
-from typing import NamedTuple, List, Any
+from typing import List, Optional
+import warnings
 
 import github
-#github.enable_console_debug_logging()
+from github.Repository import Repository
+# github.enable_console_debug_logging()
 
-from .exporthelpers.export_helper import Json
+from .exporthelpers.export_helper import setup_parser, Json, Parser
 
 
-class GithubData(NamedTuple):
-    profile: Json
+_ALL_FIELDS = [
+    'profile',
+    'events',
+    'gists',
+    'followers',
+    'following',
+    'orgs',
+    # TODO not convinced it really works? returns empty results for me
+    # but seems like github projects are a bit of a mess
+    # there are some 'classic' ones and 'new' ones for which graphql is recommended?
+    'projects',
+    'received_events',
+    'repos',
+    'starred',
+    'subscriptions',
+    'watched',
+]
 
-    events: List[Json]
-    followers: List[Json]
-    following: List[Json]
-    # TODO keys? not sure if worth keeping?
-    orgs: List[Json]
-    received_events: List[Json]
-    repos: List[Json]
-    starred: List[Json]
-    subscriptions: List[Json]
-    watched: List[Json]
 
 _TRAFFIC = 'traffic'
 
 
 class Exporter:
-    def __init__(self, *args, **kwargs) -> None:
-        kwargs['login_or_token'] = kwargs['token']
-        del kwargs['token']
+    def __init__(
+        self,
+        *args,
+        token: str,
+        include: Optional[List[str]] = None,
+        include_repos_traffic: Optional[bool] = None,
+        **kwargs,
+    ) -> None:
+        kwargs['login_or_token'] = token
         self.api = github.Github(*args, **kwargs)
+        self.fields_to_export = _ALL_FIELDS if include is None else include
+        assert len(self.fields_to_export) > 0  # just in case
+        # for now including repos traffic by default, might change in the future?
+        self.include_repos_traffic = True if include_repos_traffic is None else include_repos_traffic
 
     def export_json(self) -> Json:
         login = self.api.get_user().login
-        user = self.api.get_user(login) # need to get NamedUser first
+        user = self.api.get_user(login)  # need to get NamedUser first
 
-        fields = list(GithubData._fields)
-        fields.remove('profile')
-
-        gd = GithubData(
-            profile=user._rawData,  # type: ignore[union-attr]
-            **{f: [x._rawData for x in getattr(user, 'get_' + f)()] for f in fields},
-        )
-
-        ##  get traffic (it's only kept for 14 days :( )
-        for r in gd.repos:
-            # todo not ideal that we retrieve it all over again..
-            repo = self.api.get_repo(r['full_name'])
-
-            if repo.archived:
-                # since approx. August 2022 it started failing with
-                # github.GithubException.GithubException: 403 {"message": "Must have push access to repository",
-                # 'Traffic' tab also isn't present in the web so I guess they just arent' keeping it anymore
-                r[_TRAFFIC] = None
+        ## check that ghexport handles all avaiable data
+        for method in dir(user):
+            if not method.startswith('get_'):
                 continue
-
-            fields = ['views', 'clones', 'popular/referrers', 'popular/paths']
-            # todo ugh. this vvv doesn't quite work because returned types are different (lists vs github. objects)
-            # [x._rawData for x in getattr(repo, 'get_' + f)()]
-            # and pygithub library doesn't expose raw api properly...
-            def fetch(f: str) -> Json:
-                path = repo.url + '/traffic/' + f
-                ge = None  # type: ignore
-                attempts = 5
-                # NOTE: ugh. sometimes it just throws 500 on this endpoint for no reason, and then immediately after it works??
-                # started happening around 20220305 :shrug:
-                for attempt in range(attempts):
-                    try:
-                        return repo._requester.requestJsonAndCheck('GET', path)[1]  # type: ignore[attr-defined]
-                    except github.GithubException as ge:
-                        if ge.status != 500:
-                            raise ge
-                assert ge is not None
-                raise ge
-
-            traffic = {f: fetch(f) for f in fields}
-            assert _TRAFFIC not in r # just in case..
-            r[_TRAFFIC] = traffic
-            # TODO not sure if this is a good way to keep it...
+            if method in [
+                'get__repr__',
+                'get_repo',  # takes name and returns one repo
+                'get_organization_membership',  # only checks membership
+                'get_keys',  # these are public keys, but still not convinced it shoud be included..
+                'get_public_events',  # subset of events
+                'get_public_received_events',  # subset of received_events
+            ]:
+                continue
+            field = method[len('get_') :]
+            if field not in _ALL_FIELDS:
+                warnings.warn(f"'{field}' data isn't handled -- please report this https://github.com/karlicoss/ghexport/issues")
         ##
 
-        return gd._asdict()
+        json_data = {}
+        for f in self.fields_to_export:
+            if f == 'profile':
+                res = getattr(user, '_rawData')  # getattr makes mypy happy
+            else:
+                args = {}
+                if f == 'projects':
+                    args = {'state': 'all'}
+                objects = getattr(user, f'get_{f}')(**args)
+                res = [o._rawData for o in objects]
+                if f == 'repos' and self.include_repos_traffic:
+                    # populate repo with traffic data
+                    for repo, jr in zip(objects, res):
+                        assert _TRAFFIC not in jr, jr  # just in case..
+                        # TODO not sure if this is a good way to keep it...
+                        jr[_TRAFFIC] = self._fetch_traffic(repo=repo)
+            json_data[f] = res
+
+        return json_data
+
+    def _fetch_traffic(self, *, repo: Repository) -> Optional[Json]:
+        ##  get traffic (it's only kept for 14 days :( )
+        if repo.archived:
+            # since approx. August 2022 it started failing with
+            # github.GithubException.GithubException: 403 {"message": "Must have push access to repository",
+            # 'Traffic' tab also isn't present in the web so I guess they just arent' keeping it anymore
+            return None
+
+        traffic_fields = ['views', 'clones', 'popular/referrers', 'popular/paths']
+
+        # todo ugh. this vvv doesn't quite work because returned types are different (lists vs github. objects)
+        # [x._rawData for x in getattr(repo, 'get_' + f)()]
+        # and pygithub library doesn't expose raw api properly...
+        def fetch(f: str) -> Json:
+            path = repo.url + '/traffic/' + f
+            ge = None  # type: ignore
+            attempts = 5
+            # NOTE: ugh. sometimes it just throws 500 on this endpoint for no reason, and then immediately after it works??
+            # started happening around 20220305 :shrug:
+            for attempt in range(attempts):
+                try:
+                    return repo._requester.requestJsonAndCheck('GET', path)[1]
+                except github.GithubException as ge:
+                    if ge.status != 500:
+                        raise ge
+            assert ge is not None
+            raise ge
+
+        traffic = {f: fetch(f) for f in traffic_fields}
+        return traffic
 
 
-def get_json(**params) -> Json:
-    return Exporter(**params).export_json()
+def get_json(**kwargs) -> Json:
+    return Exporter(**kwargs).export_json()
 
 
 def main() -> None:
@@ -95,13 +134,16 @@ def main() -> None:
     params = args.params
     dumper = args.dumper
 
-    j = get_json(**params)
+    j = get_json(
+        token=params['token'],
+        include=args.include,
+        include_repos_traffic=args.include_repos_traffic,
+    )
     js = json.dumps(j, ensure_ascii=False, indent=1)
     dumper(js)
 
 
-def make_parser():
-    from .exporthelpers.export_helper import setup_parser, Parser
+def make_parser() -> argparse.ArgumentParser:
     parser = Parser('''
 Export your Github personal data: issues, PRs, comments, followers and followings, etc.
 
@@ -115,8 +157,14 @@ Export your Github personal data: issues, PRs, comments, followers and following
 You can also import ~ghexport.export~ as a module and call ~get_json~ function directly to get raw JSON.
         ''',
     )
-    return parser
+    parser.add_argument('--include', nargs='+', choices=_ALL_FIELDS, help='Only include specific export fields (exports all by default).')
 
+    tgroup = parser.add_mutually_exclusive_group()
+    tgroup.add_argument('--include-repos-traffic', dest='include_repos_traffic', action='store_true')
+    tgroup.add_argument('--exclude-repos-traffic', dest='include_repos_traffic', action='store_false')
+    parser.set_defaults(include_repos_traffic=None)
+
+    return parser
 
 
 if __name__ == '__main__':
